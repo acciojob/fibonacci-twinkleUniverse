9314548e444dae0f7decdf912b54ee7e24526537
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-07-27 17:32:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	let fibo=0,a=0,b=1;
+	if(num==0)return a;
+	if(num==1)return b;
+	for(let i=3;i<=num;i++){
+		fibo=a+b;
+		a=b;
+		b=fibo;
+	}
+	return fibo;
 }
-
+//console.log(fibonacci(4));
 module.exports = fibonacci;
